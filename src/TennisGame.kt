61929be53bae74a2555f18d311b23915f9d6c9df
JD <juandiegogarita@gmail.com>
2019-02-26 11:26:33 -0600

    update tennis game and includes getScore function

--- src/TennisGame.kt
+++ src/TennisGame.kt
@@ -1,6 +1,9 @@
-class TennisGame {
+class TennisGame(val playerOneName: String, val playerTwoName: String) {
 
     private val playerOneScore = 0
     private val playerTwoScore = 0
 
+    fun getScore(): String {
+        return "Love all"
+    }
 }
--- src/TennisGameTest.kt
+++ src/TennisGameTest.kt
@@ -1,20 +1,27 @@
 import org.junit.Before
 import org.junit.Test
 import java.lang.Exception
+import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
 class TennisGameTest {
 
-    private var tennisGame: TennisGame? = null
+    private var game: TennisGame? = null
 
     @Before
     @Throws(Exception::class)
     fun setup() {
-        tennisGame = TennisGame()
+        game = TennisGame("Foster", "Nik")
     }
 
     @Test
     fun verifyGameExists() {
-        assertNotNull(tennisGame, "the game exists")
+        assertNotNull(game, "the game exists")
+    }
+
+    @Test
+    fun testNewGameShouldReturnLoveAll() {
+        val score = game?.getScore()
+        assertEquals("Love all", score)
     }
 }
